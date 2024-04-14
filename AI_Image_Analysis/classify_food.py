ff63a85382aa631fc982e7937def4b527e6334e5
Kyae <johnkimdw@gmail.com>
2024-04-13 20:32:29 -0400

    update ml query

---
+++ AI_Image_Analysis/classify_food.py
@@ -0,0 +1,14 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/nateraw/food"
+headers = {"Authorization": "Bearer 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"}
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+
+# q = "./AI_Image_Analysis/input/lobster_roll.jpg"
+output = query(q)
+# print(output)
